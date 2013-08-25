6a4769fbd3c4f4a07c70bd37563fbb7cadac35f2
syline <sylinelee@gmail.com>
2013-08-25 21:55:09 +0200

    nouvelle BD

--- config/wandee.sql
+++ config/wandee.sql
@@ -7,44 +7,36 @@
 -- Version du serveur: 5.5.24-log
 -- Version de PHP: 5.4.3
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 --
--- Base de données: `wandee`
+-- Base de données: wandee
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `plat`
+-- Structure de la table plat
 --
-
+DROP TABLE IF EXISTS mealreserved;
 DROP TABLE IF EXISTS plat;
 DROP TABLE IF EXISTS reservation;
 DROP TABLE IF EXISTS user;
 
-CREATE TABLE IF NOT EXISTS `plat` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) COLLATE ascii_bin NOT NULL,
-  `type` varchar(10) COLLATE ascii_bin NOT NULL,
-  `description` varchar(100) COLLATE ascii_bin NOT NULL,
-  `price1` decimal(10,2) NOT NULL,
-  `price2` decimal(10,2) NOT NULL,
-  `price3` decimal(10,2) NOT NULL,
-  `piment` int(2) NOT NULL,
-  `imageLink` varchar(100) COLLATE ascii_bin NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=ascii COLLATE=ascii_bin AUTO_INCREMENT=3 ;
+
+CREATE TABLE IF NOT EXISTS plat (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(50) NOT NULL,
+  type varchar(10) NOT NULL,
+  description varchar(100) NOT NULL,
+  price1 decimal(10,2) NOT NULL,
+  price2 decimal(10,2) NOT NULL,
+  price3 decimal(10,2) NOT NULL,
+  piment int(2) NOT NULL,
+  imageLink varchar(100) NOT NULL,
+  PRIMARY KEY (id)
+);
 
 --
--- Contenu de la table `plat`
+-- Contenu de la table plat
 --
 
 INSERT INTO plat (name, type, description, price1, price2, price3, piment, imageLink) VALUES
@@ -87,30 +79,39 @@ INSERT INTO plat (name, type, description, price1, price2, price3, piment, image
 ('Jus d''orange (33cl)', 'boisson', '', '1.00', '1.00', '1.00', 0, 'photo1.jpg');
 -- --------------------------------------------------------
 
---
--- Structure de la table `reservation`
---
+CREATE TABLE IF NOT EXISTS reservation (
+  id int NOT NULL AUTO_INCREMENT,
+  nom varchar(100) NOT NULL,
+  prenom varchar(100) NOT NULL,
+  email varchar(100) NOT NULL,
+  telephone int(10) NOT NULL,
+  PRIMARY KEY (id)
+);
 
-CREATE TABLE IF NOT EXISTS `reservation` (
-  `idReservation` int(11) NOT NULL,
-  `idPlat` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `total` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=ascii COLLATE=ascii_bin;
+--
+-- Structure de la table reservation
+--
+CREATE TABLE IF NOT EXISTS mealreserved (
+  idReservation int NOT NULL,
+  idPlat int NOT NULL,
+  quantity int NOT NULL,
+  PRIMARY KEY (idReservation, idPlat)
+);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `user`
+-- Structure de la table user
 --
+CREATE TABLE IF NOT EXISTS user (
+  id int NOT NULL AUTO_INCREMENT,
+  login varchar(20) NOT NULL,
+  password varchar(50) NOT NULL,
+  PRIMARY KEY (id)
+);
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `login` varchar(20) CHARACTER SET latin1 NOT NULL,
-  `password` varchar(50) CHARACTER SET latin1 NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=ascii COLLATE=ascii_bin AUTO_INCREMENT=1 ;
+alter table mealreserved add constraint FK_idReservation_id foreign key (idReservation)
+references reservation (id) on delete restrict on update restrict;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+alter table mealreserved add constraint FK_idPlat_id foreign key (idPlat)
+references plat (id) on delete restrict on update restrict;
