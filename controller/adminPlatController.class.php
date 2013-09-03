ccc17a8ac5433fbb07cf11964184c7d08d30491d
syline <sylinelee@gmail.com>
2013-09-03 23:09:28 +0200

    début admin

--- controller/adminPlatController.class.php
+++ controller/adminPlatController.class.php
@@ -4,10 +4,51 @@ namespace controller;
 class adminPlatController extends \Library\backcontroller
 {  
 	public function executeShow() {
-		//$this->page->addVar('accueil', 'id="active"');
-		//$this->page->addVar('test', 'show');
+		$db = new \Library\DB;
+		$manager = new \model\FoodManager($db->getInstance());
+
+		$list = $manager->getAll();
+		$this->page->addVar('list', $list);
 		$this->page->setContentFile(__DIR__.'/../view/adminPlatView.php');
 	}
+
+	public function executeGetInfo() {
+		$id = $_POST['id'];
+		$db = new \Library\DB;
+		$manager = new \model\FoodManager($db->getInstance());
+		$data['info'] = $manager->getById($id);
+		header('Content-Type: application/json', true);
+		echo json_encode($data);
+	}
+	public function executeAdd() {
+		var_dump($_POST);
+		/* 'name' => string 'n' (length=1)
+  'type' => string 'entree' (length=6)
+  'description' => string 'desc' (length=4)
+  'price' => string '1.3' (length=3)
+  'spice' => string '5' (length=1)
+  'envoyer' => string 'Envoyer' (length=7)*/
+  if(isset($_FILES['file'])) { 
+  	$dossier = 'images/';
+  	$fichier = basename($_FILES['file']['name']);
+  	if(move_uploaded_file($_FILES['file']['tmp_name'], $dossier . $fichier)) {
+  		echo 'Upload effectué avec succès !';
+  	} else {
+  		echo 'Echec de l\'upload !';
+  	}
+  }
+  $this->page->setContentFile(__DIR__.'/../view/adminPlatView.php');
+}
+
+public function executeModify() {
+	echo "modifier";
+	$this->page->setContentFile(__DIR__.'/../view/adminPlatView.php');
+}
+
+public function executeDelete() {
+	echo "supprimer";
+	$this->page->setContentFile(__DIR__.'/../view/adminPlatView.php');
+}
 }
 
 ?>
--- view/adminPlatView.php
+++ view/adminPlatView.php
@@ -1,1 +1,77 @@
-coucou ! 
+<div id="ajouter">
+	<h3>Ajouter un plat</h3>
+	<!-- Ajouter un plat -->
+	<form method=POST action="/wandee/adminPlat/add/" class="form-horizontal" enctype="multipart/form-data">
+		<div class="control-group">
+			<label class="control-label" for="name">Nom : </label>
+			<div class="controls">
+				<input required id="name" name="name" type="text" placeholder="Nom"></input>
+			</div>
+		</div>
+		<div class="control-group">
+			<label class="control-label" for="type">Nom : </label>
+			<div class="controls">
+				<select required name="type" id="type">
+					<option value="entree">Entrée</option>
+					<option value="plat">Plat</option>
+					<option value="dessert">Dessert</option>
+				</select>
+			</div>
+		</div>
+		<div class="control-group">
+			<label class="control-label" for="description">Description : </label>
+			<div class="controls">
+				<input id="description" name="description" type="text" placeholder="Description"></input>
+			</div>
+		</div>
+		<div class="control-group">
+			<label class="control-label" for="price">Prix : </label>
+			<div class="controls">
+				<input required id="price" name="price" type="text" placeholder="Prix"></input>
+			</div>
+		</div>
+		<div class="control-group">
+			<label class="control-label" for="spice">Piment : </label>
+			<div class="controls">
+				<input required id="spice" name="spice" type="text" placeholder="Nombre de piment(s)"></input>
+			</div>
+		</div>
+		<div class="control-group">
+			<label class="control-label" for="file">Image : </label>
+			<div class="controls">
+				<input data-bfi-disabled title="choisir un fichier" type="file" name="file"></input>
+			</div>
+		</div>	
+
+		<input class="btn" type="submit" name="envoyer" value="Envoyer"></input>
+	</form>
+</div>
+<div id="modifier">
+	<h3>Modifier un plat</h3>
+	<!-- <form method=POST action="/wandee/adminPlat/modify/" class="form-horizontal"> -->
+	<select name="idModify" onChange='chargerInfo()'>
+		<?php 
+		foreach ($list as $key => $value) {
+			echo "<option value=" . $value['id'] . ">" . $value['name'] . "</option>";
+		}
+		?>
+	</select>
+</div>
+
+<script>
+	function chargerInfo() {
+		var id = $('[name=idModify]').val();
+		console.log(id);
+		$.ajax({
+			type : 'POST',
+			url : '/wandee/adminPlat/getInfo',
+			dataType : 'json',
+			data : {
+				id : id,
+			},
+			success : function(data){
+				console.log(data);
+			}
+		});
+	}
+</script>
